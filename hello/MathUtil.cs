0b07de9b6ecda73aa644720b16dc83c17ec69520
rehmanhamza <hamzarehman13@gmail.com>
2022-03-01 15:47:45 +0100

    dotNET Lessons

---
+++ hello/MathUtil.cs
@@ -0,0 +1,5 @@
+﻿namespace MathLibrary;
+
+public class MathUtil{
+    public static int Add(int x, int y) => x+y;
+}
---
+++ hello/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// change from debug to release to ship the final software
+
+using MathLibrary;
+
+namespace HelloDotNet{
+    class Program{
+        static void Main(String[] args){
+            Console.WriteLine($"The answer is {MathUtil.Add(2,2)}");
+
+            //Console.WriteLine("Hello");
+        }
+    }
+}
