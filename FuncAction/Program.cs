f563d11e4fb7b8e4c69e32da5481f4c227182efb
rehmanhamza <hamzarehman13@gmail.com>
2022-03-02 12:11:50 +0100

    dotNET Lessons

---
+++ FuncAction/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Diagnostics;
+
+// 1
+var watch = Stopwatch.StartNew();
+CountToInfinity(); // Method to Benchmark
+watch.Stop();
+Console.WriteLine(watch.Elapsed);
+
+// 2
+MeasureTime(() => CountToInfinity());
+
+
+// Action --> delegate void Something()
+// it's return type is void, it returns nothing
+// 2
+static void MeasureTime(Action a){
+    var watch = Stopwatch.StartNew();
+    a(); // Method to Benchmark
+    watch.Stop();
+    Console.WriteLine(watch.Elapsed);
+}
+
+// 1
+static void CountToInfinity(){
+    for(var i=0; i<1000; i++);
+}
