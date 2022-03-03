f89071d484ec7a5e50abb55044212b2b9b6dca8c
rehmanhamza <hamzarehman13@gmail.com>
2022-03-03 11:28:22 +0100

    dotNET Lessons

---
+++ Closure/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 2
+// Factoring pattern
+Func<int, int> cal = CreateCalculator();
+Console.WriteLine(cal(4));
+
+// 1
+int CalculateSomething(int n){
+    if(n == 0) 
+        return 0;
+
+    var factor = 4;
+    return factor * CalculateSomething(n - 1);
+}
+
+// 2
+// returns a delegate
+Func<int, int > CreateCalculator(){
+    var factor = 4;
+    return n => n * factor;
+}
