e2994304f5d38ad631ea5e47c1c6f42f30255c88
rehmanhamza <hamzarehman13@gmail.com>
2022-03-07 16:13:19 +0100

    dotNET Lessons

--- TypeSystem/Program.cs
+++ TypeSystem/Program.cs
@@ -2,3 +2,13 @@
 using System;
 
 Console.WriteLine("Hello, World!");
+
+// better use smallcase int, string etc
+int x = 12;
+// this one is not recommended
+Int32 y = 13;
+
+// strings in C# are immutable
+var name = "Hamza";
+name = name.ToUpper();
+Console.WriteLine(name);
