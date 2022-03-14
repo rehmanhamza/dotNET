d21f3616c5f77baa1a8886de2b8d48150fb132a8
rehmanhamza <hamzarehman13@gmail.com>
2022-03-14 20:26:08 +0100

    dotNET Lessons

---
+++ Async/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+var lines = File.ReadAllLines("text.txt");
+
+foreach(var line in lines){
+    Console.WriteLine(line);
+}
